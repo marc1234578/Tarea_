7c3fdddc537f06ebba2e4a72ae030db80f0f4f99
Marcos Torres Baras <143344381+marc1234578@users.noreply.github.com>
2023-08-28 17:43:39 -0500

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿
+            Console.WriteLine("Ingrese el primer número: ");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número: ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el tercer número: ");
+            int numero3 = int.Parse(Console.ReadLine());
+
+            int mayor = CalcularMayor(numero1, numero2, numero3);
+
+            Console.WriteLine($"El mayor de los tres números es: {mayor}");
+        
+
+        static int CalcularMayor(int num1, int num2, int num3)
+        {
+            int mayor = num1;
+
+            if (num2 > mayor)
+            {
+                mayor = num2;
+            }
+
+            if (num3 > mayor)
+            {
+                mayor = num3;
+            }
+
+            return mayor;
+        }
+    
+
---
+++ Problema 10/Program.cs
@@ -0,0 +1,26 @@
+﻿
+            Console.Write("Ingrese el monto del consumo: ");
+            double montoConsumo = double.Parse(Console.ReadLine());
+
+            double descuento, impuesto, subtotal, total;
+
+            if (montoConsumo <= 100.00)
+            {
+                descuento = montoConsumo * 0.10;
+            }
+            else
+            {
+                descuento = montoConsumo * 0.20;
+            }
+
+            impuesto = montoConsumo * 0.18;
+            subtotal = montoConsumo - descuento;
+            total = subtotal + impuesto;
+
+            Console.WriteLine($"Monto del descuento: S/ {descuento:F2}");
+            Console.WriteLine($"Impuesto: S/ {impuesto:F2}");
+            Console.WriteLine($"Subtotal: S/ {subtotal:F2}");
+            Console.WriteLine($"Importe a pagar: S/ {total:F2}");
+        
+    
+
